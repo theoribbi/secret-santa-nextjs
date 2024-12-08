9582b2c466400c5da0349f5125f6191f53e519e2
Theo2903 <theo@ribbi.ch>
2024-12-08 23:01:35 +0100

    Update forms ui with Dialog and loader

--- app/edition/[id]/page.tsx
+++ app/edition/[id]/page.tsx
@@ -7,6 +7,15 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Gift, Mail, Image as ImageIcon, Check } from "lucide-react";
 import { Person } from "@prisma/client";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Loader2 } from "lucide-react";
 
 type Edition = {
   id: string;
@@ -23,12 +32,15 @@ export default function EditionPage({ params }: { params: { id: string } }) {
   const [giftIdeas, setGiftIdeas] = useState("");
   const [imageUrl, setImageUrl] = useState("");
   const inputFileRef = useRef<HTMLInputElement>(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchEdition = async () => {
       const response = await fetch(`/api/editions/${params.id}`);
       const data = await response.json();
       setEdition(data);
+      setIsLoading(false);
     };
     fetchEdition();
   }, [params.id]);
@@ -42,6 +54,8 @@ export default function EditionPage({ params }: { params: { id: string } }) {
 
   const handleSubmit = async () => {
     if (!selectedPerson) return;
+
+    setIsSubmitting(true);
 
     const formData = new FormData();
     formData.append("personId", selectedPerson.id);
@@ -81,6 +95,8 @@ export default function EditionPage({ params }: { params: { id: string } }) {
       setSelectedPerson(null);
     } catch (error) {
       console.error("Failed to update person:", error);
+    } finally {
+      setIsSubmitting(false);
     }
   };
 
@@ -102,7 +118,7 @@ export default function EditionPage({ params }: { params: { id: string } }) {
     (person: Person) => person.email && person.giftIdeas
   );
 
-  if (!edition) return <div>Loading...</div>;
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100 py-12 px-4 sm:px-6 lg:px-8">
@@ -110,20 +126,20 @@ export default function EditionPage({ params }: { params: { id: string } }) {
         <div className="text-center mb-12">
           <Gift className="mx-auto h-16 w-16 text-primary" />
           <h1 className="mt-6 text-4xl font-bold text-gray-900">
-            {edition.name}
+            {edition?.name}
           </h1>
           <p className="mt-2 text-lg text-gray-600">
-            {edition.status === "COMPLETED"
+            {edition?.status === "COMPLETED"
               ? "Les attributions du Secret Santa ont été envoyées !"
               : "Cliquez sur votre nom pour ajouter vos informations"}
           </p>
         </div>
 
-        <div className="grid md:grid-cols-2 gap-6">
+        <div className="max-w-xl mx-auto grid sm:grid-cols-1 gap-6">
           <Card className="p-6">
             <h2 className="text-xl font-semibold mb-4">Participants</h2>
             <div className="space-y-2">
-              {edition.people.map((person: Person) => {
+              {edition?.people.map((person: Person) => {
                 const isPersonCompleted = !!person.email && !!person.giftIdeas;
 
                 return (
@@ -143,7 +159,7 @@ export default function EditionPage({ params }: { params: { id: string } }) {
               })}
             </div>
 
-            {edition.status === "PENDING" && (
+            {edition?.status === "PENDING" && (
               <Button
                 className="w-full mt-6"
                 onClick={handleDraw}
@@ -153,60 +169,70 @@ export default function EditionPage({ params }: { params: { id: string } }) {
               </Button>
             )}
           </Card>
-
-          {selectedPerson && (
-            <Card className="p-6">
-              <h2 className="text-xl font-semibold mb-4">
-                Détail de {selectedPerson.name}
-              </h2>
-              <div className="space-y-4">
-                <div>
-                  <label className="block text-sm font-medium text-gray-700">
-                    <Mail className="inline-block w-4 h-4 mr-2" />
-                    Email
-                  </label>
-                  <Input
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="ton@email.com"
-                    className="mt-1"
-                  />
-                </div>
-
-                <div>
-                  <label className="block text-sm font-medium text-gray-700">
-                    <Gift className="inline-block w-4 h-4 mr-2" />
-                    Idées de cadeaux
-                  </label>
-                  <Textarea
-                    value={giftIdeas}
-                    onChange={(e) => setGiftIdeas(e.target.value)}
-                    placeholder="Partagez vos idées de cadeaux..."
-                    className="mt-1"
-                  />
-                </div>
-
-                <div>
-                  <label className="block text-sm font-medium text-gray-700">
-                    <ImageIcon className="inline-block w-4 h-4 mr-2" />
-                    Image (optional)
-                  </label>
-                  <Input
-                    ref={inputFileRef} // Référence de l'input file
-                    type="file"
-                    accept="image/*"
-                    className="mt-1"
-                  />
-                </div>
-
-                <Button onClick={handleSubmit} className="w-full">
-                  Save Details
-                </Button>
-              </div>
-            </Card>
-          )}
         </div>
+
+        <Dialog
+          open={!!selectedPerson}
+          onOpenChange={() => setSelectedPerson(null)}
+        >
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Détail de {selectedPerson?.name}</DialogTitle>
+              <DialogDescription>
+                Complétez les informations suivantes pour participer.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="space-y-4">
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  <Mail className="inline-block w-4 h-4 mr-2" />
+                  Email
+                </label>
+                <Input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="ton@email.com"
+                  className="mt-1"
+                />
+              </div>
+
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  <Gift className="inline-block w-4 h-4 mr-2" />
+                  Idées de cadeaux
+                </label>
+                <Textarea
+                  value={giftIdeas}
+                  onChange={(e) => setGiftIdeas(e.target.value)}
+                  placeholder="Partagez vos idées de cadeaux..."
+                  className="mt-1"
+                />
+              </div>
+
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  <ImageIcon className="inline-block w-4 h-4 mr-2" />
+                  Image (optionnelle)
+                </label>
+                <Input
+                  ref={inputFileRef}
+                  type="file"
+                  accept="image/*"
+                  className="mt-1"
+                />
+              </div>
+
+              <Button onClick={handleSubmit} className="w-full mt-4">
+                {isSubmitting ? (
+                  <Loader2 className="animate-spin h-5 w-5 text-white" />
+                ) : (
+                  "Envoyer"
+                )}
+              </Button>
+            </div>
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
   );
