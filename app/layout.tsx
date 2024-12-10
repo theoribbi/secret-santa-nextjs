7f04540cd3a96f7b3b42c6d5ffe8b6e472eae8e3
Theo2903 <theo@ribbi.ch>
2024-12-10 13:32:31 +0100

    Add Toaster on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Toaster } from "@/components/ui/toaster";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Secret Santa 🎁',
-  description: 'A Free Secret Santa Generator',
+  title: "Secret Santa 🎁",
+  description: "A Free Secret Santa Generator",
 };
 
 export default function RootLayout({
@@ -15,8 +16,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="fr">
+      <body className={inter.className}>
+        {children}
+        <Toaster />
+      </body>
     </html>
   );
 }
